6961ee98f3c48e52c236fd6d49860080cd78f121
MONSTER B <dvnv36@mail.ru>
2023-02-06 19:44:06 +0300

    Исправила, проверь пожалуйста

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,6 @@ public class Main {
         Author author1 = new Author( "Лев", " Толстой");
         Book book1 = new Book(" Война и мир", author1,1867);
 
-
         System.out.printf("%s автор %s %s, %d%n" ,
                 book1.getBookName(),
                 book1.getAuthor().getFirstName(),
